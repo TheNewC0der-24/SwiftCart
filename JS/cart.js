26e3828eefd0403fd58aba73c3938cd8aef7a4aa
Bhavya Khurana <bhavyakhurana002@gmail.com>
2023-06-24 03:34:39 +0530

    Fetch data and add functionality for add items and delete items

--- JS/cart.js
+++ JS/cart.js
@@ -1,52 +1,67 @@
 async function getProducts() {
-    const response = await fetch('https://dummyjson.com/products');
-    const products = await response.json();
 
-    let output = '';
+    try {
+        const response = await fetch('https://dummyjson.com/products');
+        const products = await response.json();
 
-    let cartItem = JSON.parse(localStorage.getItem('cartItem'));
-    let cart = products.products.filter(function (product) {
-        return cartItem.includes(product.id);
-    })
+        let output = '';
+        let totalPrice = 0;
 
-    cart.forEach(function (product) {
-        let qty = 0;
-        cartItem.sort();
-        cartItem.reverse();
-        while (cartItem.includes(product.id) === true) {
-            qty++;
-            for (var i = 0; i < cartItem.length; i++) {
-                if (cartItem[i] === product.id) {
-                    cartItem = removeItemWithSlice(i, cartItem);
-                    break;
+        let cartItem = JSON.parse(localStorage.getItem('cartItem'));
+        let cart = products.products.filter(function (product) {
+            return cartItem.includes(product.id);
+        })
+
+        cart.forEach(function (product) {
+            let qty = 0;
+            cartItem.sort();
+            cartItem.reverse();
+            while (cartItem.includes(product.id) === true) {
+                qty++;
+                for (var i = 0; i < cartItem.length; i++) {
+                    if (cartItem[i] === product.id) {
+                        cartItem = removeItemWithSlice(i, cartItem);
+                        break;
+                    }
                 }
             }
-        }
 
-        output += `
-            <div class="products-card" id=${product.id}>
-                    <div class="products-card-body">
-                        <img src=${product.thumbnail} alt="product">
-                        <h3>Product Name</h3>
-                        <div class="products-card-footer">
-                            <div class="price">
-                                $50.00
-                            </div>
+            totalPrice += product.price * qty;
+
+            output += `
+                <tr>
+                    <th scope="row">${product.id}</th>
+                    <td>
+                        <img src=${product.thumbnail} class="img-fluid cart-thumbnail" alt="Product_Image">
+                    </td>
+                    <td>${product.title}</td>
+                    <td>$${product.price}</td>
+                    <td>
+                        <div class="d-flex m-auto align-items-center gap-2">
+                            <button class="btn btn-outline-danger" onclick="removeFromCart(${product.id})">
+                                <i class="fa-solid fa-minus"></i>
+                            </button>
+                            <button class="btn" style="background-color: #6610f2; color: #fff;">${qty}</button>
+                            <button class="btn btn-success" onclick="addToCart(${product.id})">
+                                <i class="fa-solid fa-plus"></i>
+                            </button>
                         </div>
-                        <div>Quantity: ${qty}</div>
-                        <button onclick="removeFromCart(${product.id})">Remove</button>
-                        <button onclick="addToCart(${product.id})">Add</button>
-                    </div>
-            </div>
+                    </td>
+                    <td>$${(product.price * qty).toFixed(2)}</td>
+                </tr>
         `;
-    });
+        });
 
-    document.getElementById('cartData').innerHTML = output;
+        document.getElementById('tbody').innerHTML = output;
+        document.getElementById('totalPrice').textContent = `$${totalPrice.toFixed(2)}`;
+    } catch (error) {
+        console.error('Error fetching product details:', error);
+    }
 }
 
 getProducts();
 
-document.getElementById('cartQuantity').innerHTML = JSON.parse(localStorage.getItem('cartItem')).length;
+document.getElementById('cartItem').innerHTML = JSON.parse(localStorage.getItem('cartItem')).length;
 
 function removeFromCart(id) {
     let cart = JSON.parse(localStorage.getItem('cartItem'));
@@ -57,7 +72,8 @@ function removeFromCart(id) {
         }
     }
     localStorage.setItem('cartItem', JSON.stringify(cart));
-    document.getElementById('cartQuantity').innerHTML = JSON.parse(localStorage.getItem('cartItem')).length;
+    document.getElementById('cartItem').innerHTML = JSON.parse(localStorage.getItem('cartItem')).length;
+    document.getElementById('cartQuantity').textContent = JSON.parse(localStorage.getItem('cartItem')).length;
     getProducts();
 }
 
@@ -67,7 +83,8 @@ function addToCart(id) {
     cart.reverse();
     cart.push(id);
     localStorage.setItem('cartItem', JSON.stringify(cart));
-    document.getElementById('cartQuantity').innerHTML = JSON.parse(localStorage.getItem('cartItem')).length;
+    document.getElementById('cartItem').innerHTML = JSON.parse(localStorage.getItem('cartItem')).length;
+    document.getElementById('cartQuantity').textContent = JSON.parse(localStorage.getItem('cartItem')).length;
     getProducts();
 }
 
@@ -75,8 +92,11 @@ function removeItemWithSlice(index, items) {
     return [...items.slice(0, index), ...items.slice(index + 1)]
 }
 
+document.getElementById('cartQuantity').textContent = JSON.parse(localStorage.getItem('cartItem')).length;
+
 const clearCart = () => {
     localStorage.setItem('cartItem', JSON.stringify([]));
-    document.getElementById('cartQuantity').innerHTML = JSON.parse(localStorage.getItem('cartItem')).length;
+    document.getElementById('cartItem').innerHTML = JSON.parse(localStorage.getItem('cartItem')).length;
+    document.getElementById('cartQuantity').textContent = JSON.parse(localStorage.getItem('cartItem')).length;
     getProducts();
 };
